8c7ad187908ca8c71c280702ecf3a190ddfd75a7
adam james <adam.james@binus.ac.id>
2024-12-15 10:04:29 +0700

    #feat top UI

--- app/src/main/java/com/example/aol_mobileprogramming/ui/bottomnav/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/aol_mobileprogramming/ui/bottomnav/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.aol_mobileprogramming.ui.bottomnav.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.aol_mobileprogramming.databinding.FragmentHomeBinding;
+import com.example.aol_mobileprogramming.ui.cart.CartActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -19,6 +22,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        binding.cartButton.setOnClickListener(v -> {
+            Intent in = new Intent(requireContext(), CartActivity.class);
+            startActivity(in);
+        });
         return root;
     }
 
