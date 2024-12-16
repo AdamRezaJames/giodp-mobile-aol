8d86217f02ae90066a98114daa78dfd00de4f347
adam james <adam.james@binus.ac.id>
2024-12-16 16:48:55 +0700

    #feat show username in hommfragment

--- app/src/main/java/com/example/aol_mobileprogramming/ui/bottomnav/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/aol_mobileprogramming/ui/bottomnav/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.aol_mobileprogramming.ui.bottomnav.ui.home;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +22,7 @@ import com.example.aol_mobileprogramming.databinding.FragmentHomeBinding;
 import com.example.aol_mobileprogramming.ui.cart.CartActivity;
 import com.example.aol_mobileprogramming.ui.db.DBManager;
 import com.example.aol_mobileprogramming.ui.models.Course;
+import com.example.aol_mobileprogramming.ui.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +39,20 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        dbManager = new DBManager(getContext());
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", getContext().MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("user_id", -1);
+
+        if (userId != -1) {
+            User user = dbManager.getUser(userId);
+            if (user != null) {
+                binding.userNameText.setText(user.getName());
+            }
+        } else {
+            binding.userNameText.setText("Guest");
+        }
+
         binding.cartButton.setOnClickListener(v -> {
             Intent in = new Intent(requireContext(), CartActivity.class);
             startActivity(in);
@@ -44,7 +60,7 @@ public class HomeFragment extends Fragment {
 
         recyclerViewLayoutHome = binding.recyclerViewLayoutHome;
         recyclerViewLayoutHome.setLayoutManager(new LinearLayoutManager(getContext()));
-        CourseAdapter adapter = new CourseAdapter(new DBManager(getContext()).getCourses());
+        CourseAdapter adapter = new CourseAdapter(dbManager.getCourses());
         recyclerViewLayoutHome.setAdapter(adapter);
 
         return root;
